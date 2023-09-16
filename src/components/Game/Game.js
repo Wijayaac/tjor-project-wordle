4d04532e7ca800c9b969c0312010d4b01552bbc7
Wijayaac <wijayaac25@gmail.com>
2023-09-16 19:41:16 +0800

    Add : exercise 4 solution

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { range, sample } from "../../utils";
 import { WORDS } from "../../data";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { checkGuess } from "../../game-helpers";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -19,9 +20,8 @@ function Game() {
   };
   return (
     <>
-      <Guess results={results} />
-      <GuessInput addResults={addResults} />
       <GuessResults results={results} />
+      <GuessInput addResults={addResults} />
     </>
   );
 }
@@ -38,7 +38,7 @@ function GuessInput({ addResults }) {
   return (
     <form className='guess-input-wrapper' onSubmit={handleSubmit}>
       <label htmlFor='guess-input'>Enter guess:</label>
-      <input id='guess-input' type='text' value={guess} maxLength={5} pattern='[a-Z]{2}' onChange={(event) => setGuess(event.target.value)} />
+      <input id='guess-input' type='text' value={guess} maxLength={5} minLength={5} pattern='[a-zA-Z]{5}' onChange={(event) => setGuess(event.target.value)} title='5 letter word' />
     </form>
   );
 }
@@ -51,24 +51,23 @@ function GuessResults({ results }) {
           {result.guess}
         </p>
       ))}
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess value={results[num]} key={num} />
+      ))}
     </div>
   );
 }
-function Guess({ results }) {
-  console.log(results);
+
+function Guess({ value }) {
+  const guessCheck = value ? checkGuess(value.guess, answer) : undefined;
   return (
-    <div className='guess-results'>
-      {range(NUM_OF_GUESSES_ALLOWED).map((value) => (
-        <p className='guess' key={value}>
-          {results[value] &&
-            results[value].guess.split("").map((letter, index) => (
-              <span className='cell' key={index}>
-                {letter}
-              </span>
-            ))}
-        </p>
+    <p className='guess' key={value}>
+      {range(5).map((num) => (
+        <span className={`cell ${guessCheck && guessCheck[num].status}`} key={num}>
+          {value ? value.guess[num] : undefined}
+        </span>
       ))}
-    </div>
+    </p>
   );
 }
 export default Game;
